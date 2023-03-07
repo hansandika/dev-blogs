5a23f1a6f6de27efb851829d49242f3e81639556
hansandika <hansandika70@gmail.com>
2023-03-07 16:59:48 +0700

    add content to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-gray-400">
-      Hello world!
-    </h1>
+    <div>
+      <Head>
+        <title>Blog App</title>
+        <meta name="description" content="Blog App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className="text-3xl font-bold">
+        Blog App
+      </h1>
+      <Link href={'/blogs'} className="underline hover:text-red-500">Click here to Blog page</Link>
+    </div>
   )
 }
 
